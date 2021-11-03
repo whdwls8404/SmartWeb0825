90f58f08ea19c297344c94f077e3a36a9de5d2d8
koreaitlecture <min-k-t@hanmail.net>
2021-11-03 16:05:26 +0900

    2021-11-03

--- jspstudy/workspace/03_NAVER_API/src/login/LoginServlet.java
+++ jspstudy/workspace/03_NAVER_API/src/login/LoginServlet.java
@@ -1,7 +1,6 @@
 package login;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,19 +15,14 @@ public class LoginServlet extends HttpServlet {
         super();
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String captchaKey = GetKey.getKey();
-		
+		String key = GetKey.getKey();
+		GetImage.getImage(key, request);
+		request.setAttribute("key", key);
+		// 로그인 화면으로 가서 이미지 보여줘야 함
+		// 포워드 : request 가지고 이동 - request에 파일명 저장
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 }
-
-
-
-
-
-
-
-
-
