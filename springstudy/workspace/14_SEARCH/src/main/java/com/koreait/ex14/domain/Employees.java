c86ecbec536347dc05fc99b25f531fb414196484
koreaitlecture <min-k-t@hanmail.net>
2021-12-14 11:05:06 +0900

    2021-12-14

---
+++ springstudy/workspace/14_SEARCH/src/main/java/com/koreait/ex14/domain/Employees.java
@@ -0,0 +1,32 @@
+package com.koreait.ex14.domain;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Employees {
+
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private Date hireDate;
+	private String jobId;
+	private int salary;
+	private double commissionPct;
+	private int managerId;
+	private int departmentId;
+	
+}
