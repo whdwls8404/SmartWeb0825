f7b7856efadde2de20c48e1913d6a090fc59c9f8
koreaitlecture <min-k-t@hanmail.net>
2021-12-16 09:09:09 +0900

    2021-12-16

--- springstudy/workspace/14_SEARCH/src/main/java/com/koreait/ex14/config/MyBatisConfig.java
+++ springstudy/workspace/14_SEARCH/src/main/java/com/koreait/ex14/config/MyBatisConfig.java
@@ -3,23 +3,31 @@ package com.koreait.ex14.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@PropertySource("classpath:mybatis/properties/db.properties")
 public class MyBatisConfig {
-
+	
+	@Value("${hikariConfig.driverClassName}") private String driverClassName;
+	@Value("${hikariConfig.jdbcUrl}") private String jdbcUrl;
+	@Value("${hikariConfig.username}") private String username;
+	@Value("${hikariConfig.password}") private String password;
+	
 	@Bean
 	public HikariConfig hikariConfig() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
-		hikariConfig.setUsername("HR");
-		hikariConfig.setPassword("1111");
+		hikariConfig.setDriverClassName(driverClassName);
+		hikariConfig.setJdbcUrl(jdbcUrl);
+		hikariConfig.setUsername(username);
+		hikariConfig.setPassword(password);
 		return hikariConfig;
 	}
 	
@@ -33,12 +41,7 @@ public class MyBatisConfig {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(hikariDataSource());
 		sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml"));
-		sqlSessionFactoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:mybatis/mybatis-config.xml"));
-		/*
-		Properties properties = new Properties();
-		properties.put("mapUnderscoreToCamelCase", true);
-		sqlSessionFactoryBean.setConfigurationProperties(properties);
-		*/
+		sqlSessionFactoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:mybatis/config/mybatis-config.xml"));
 		return sqlSessionFactoryBean.getObject();
 	}
 	
